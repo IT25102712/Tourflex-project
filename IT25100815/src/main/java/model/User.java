fd451b48148003198285d8758aefc13dad18235c
Simraameen <it25100815@my.sliit.lk>
2026-05-12 13:40:18 +0530

    Add files via upload

--- IT25100815/src/main/java/model/User.java
+++ IT25100815/src/main/java/model/User.java
@@ -11,14 +11,31 @@ public class User {
     private int id;
 
     private String name;
+
+    @Column(unique = true, nullable = false)
     private String email;
     private String password;
+
+    @Column(unique = true, nullable = false)
+    private String phone;
+
+    private String address;
+
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] image;
+
+    // Getters and Setters
+    public byte[] getImage() { return image; }
+    public void setImage(byte[] image) { this.image = image; }
 
     public User() {}
 
     public int getId() {
         return id;
     }
+    public void setId(int id) {
+        this.id = id; }
 
     public String getName() {
         return name;
@@ -42,5 +59,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
