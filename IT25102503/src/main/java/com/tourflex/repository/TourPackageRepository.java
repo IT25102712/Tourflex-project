91d5818b0ed6ec6be0a3dd00abd9731caefd4765
IT25102503 <IT25102503@my.sliit.lk>
2026-05-15 06:42:23 +0530

    Add files via upload

---
+++ IT25102503/src/main/java/com/tourflex/repository/TourPackageRepository.java
@@ -0,0 +1,9 @@
+package com.tourflex.repository;
+
+import com.tourflex.model.TourPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TourPackageRepository extends JpaRepository<TourPackage, Integer> {
+    TourPackage findByName(String name);
+}
+//IT25102503
